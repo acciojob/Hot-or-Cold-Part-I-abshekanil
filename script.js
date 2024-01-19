49c6d59cb94463d8ac4a95a947ebd447b0f1eecc
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-19 11:39:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 //your code here
+const button = document.getElementById('btn');
+let guessNumber = document.getElementById('guess').value;
+let response = document.getElementById('response');
+let respondHotOrCold = document.getElementById('respond');
+let randomNumber = Math.random()*100;
+let previousGuess = parseInt(guessNumber);
+
+button.addEventListener('click', function(){
+
+	let difference = parseInt(guessNumber) - previousGuess;
+		
+	if(parseInt(guessNumber) < randomNumber)
+	{
+		response.innerHTML = 'guess higher';
+		 
+		if(difference < (parseInt(guessNumber) - randomNumber))
+		{
+			respondHotOrCold.innerHTML = 'Getting cooler';
+		}
+		elseif(difference > (parseInt(guessNumber) - randomNumber))
+		{
+			respondHotOrCold.innerHTML = 'Getting hotter';
+		}
+	}
+	else if(parseInt(guessNumber) > randomNumber)
+	{
+		response.innerHTML = 'guess lower';
+	}
+	
+});
+
 
